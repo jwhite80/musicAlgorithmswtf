4dfe9dd9afe169c5aec3be23da21a874d41251e9
RisouKenshi <samplenhold@hotmail.com>
2015-10-26 18:05:57 -0700

    Updated normalization
    Isaac here. I fixed the division class to produce a more realistic sine function output once the translation has occurred. I also fixed modulo after it was broken by allowing the negative values in the raw data array.

--- CSCD480something/MusicAlgorithmBinder/8. Source Code/js/normalization.js
+++ CSCD480something/MusicAlgorithmBinder/8. Source Code/js/normalization.js
@@ -6,53 +6,134 @@ var Division = function(){
 	
 	this.normalize = function(data,minValue,maxValue)
 	{
-		var  dataShift;
-		var _value = new Array();
-		var dataMin = Math.min.apply(Math,data);
-		var dataMax = Math.max.apply(Math,data);
-		var dataRange = dataMax - dataMin;
-		var newRange =  maxValue - minValue;
-		var rangeScale;
-		
-		if(dataRange == 0){
-			rangeScale = newRange / 1;
-		}
-		else{
-			rangeScale = newRange / dataRange;
-		}		
-		
+	    var resolution = 0; // This is the variable used for creating the translation list.
+	    var translationList = new Array();// This array contains a translationlist with original values mapped to translated values.
+	    var finalTranslation = new Array();// This is the data array translated to the new translated values.
 
-		for(var i=0; i< data.length; i++){
-			dataShift = data[i] - dataMin;
-			_value.push((Math.round(dataShift * rangeScale) + +minValue));	
-		}		
+	    var keyboardRange = Range(minValue, maxValue); // This is the range used to determine how many keys are playable.
+	    var algorithmInput = 0;// This is a variable that gets range of variation from the data array.
 
-		return _value;
-	};	
+	    var minimumDataValue = Math.min.apply(Math, data);
+	    var maximumDataValue = Math.max.apply(Math, data);
+	    
+	    if (minimumDataValue < 0)
+	    {// if there are negative values, this is called. It's a special case.
+	        algorithmInput = NegativeRange(minimumDataValue, maximumDataValue);
+	    }
+	    else
+	    {
+	        algorithmInput = PositiveRange(minimumDataValue, maximumDataValue);
+	    }
+
+	    if(maximumDataValue <= maxValue && algorithmInput < keyboardRange)
+	    {
+	        resolution = keyboardRange / algorithmInput;
+
+	        if(resolution === 0)
+	        {// if the resolution is 0, it must change to 1 in order to make the translation happen. 
+	            resolution = 1;
+	        }
+	    }
+	    else
+	    {
+	        resolution = keyboardRange / algorithmInput;
+	    }
+
+	    translationList = CreateTranslationTable(minimumDataValue, algorithmInput, resolution); // This creates the translation table array.
+	    finalTranslation = CreateFinalList(data,translationList);// this provides the final output.
+	    
+	    return finalTranslation;
+	};
+	
+	/*
+	This method creates the final list. It utilizes a Finder method that takes the raw data and matches values in the translation 
+	table and moves the translation value to a new array that will be returned as finalTranslation.
+	*/
+	function CreateFinalList(originalList,translatedList)
+	{
+	    var temporaryList = new Array();
+
+	    for(var x = 0; x < originalList.length; x++)
+	    {
+	        temporaryList.push(parseInt(Finder(translatedList,originalList[x])));
+	    }
+	    return temporaryList;
+	}
+	/*
+	Finder is used to help create the finalTranslation array.
+	*/
+	function Finder(translatedTable, value)
+	{
+	    var counter = 0;
+
+	    while(value !== translatedTable[counter].originalValue)
+	    {
+	        counter++;
+	    }
+
+	    return translatedTable[counter].translatedValue;
+	}
+	/*
+	This creates a list of all possible values in the range specified by the raw data array. It assigns a value to each 
+	of these values and then an object is created to contain dependant values. Original -> Translation.
+	*/
+	function CreateTranslationTable(min, range, unit)
+	{
+	    var temporaryList = new Array();
+	    incrementor = min;
+
+	    for(var x = 0; x < range; x++)
+	    {
+	        temporaryList.push(new TranslationKey(incrementor, x * unit));
+	        incrementor++;
+	    }
+	    return temporaryList;
+	}
+    /*
+        Range method gets range from keyboard and duration values.
+    */
+	function Range(min, max) {
+	    return (max - min) + 1;
+	}
+
+    /*
+        NegativeRange and PositiveRange methods are for building translation table.
+    */
+	function NegativeRange(min, max){
+	    return (min * -1) + (max + 1);
+	}
+
+	function PositiveRange(min, max){
+	    return (max + 1) - min;
+	}
 };
 
+/*
+Translation object for division class. original is the key, translatedvalue is new value per
+constraints.
+*/
+function TranslationKey(original, translation) {
+    this.originalValue = original;
+    this.translatedValue = translation;
+}
 
 //Modulo class
 var Modulo = function(){
 	
 	this.normalize = function(data,minValue,maxValue)
 	{	
-		//may need a check		
-		var moddedData;
-		var _value = new Array();		
-		var modNum = maxValue - minValue + 1;
-
+		var _value = new Array();// the array that will contained modded values 		
+		var modNum = maxValue - minValue + 1;// This is the value retrieved from the keyboard or duration values. It is the max basically.
+		
 		for(var i = 0; i < data.length; i++)
-		{ 
-			moddedData = parseInt(data[i] % modNum);
-			if(moddedData < 0)
-			{
-				_value.push(maxValue + +moddedData);
-			}
-			else
-			{
-				_value.push(moddedData + +minValue);
-			}
+		{
+		    if(data[i] < 0)
+		    {// This deals with values that are negative. Technically, negative values can't be modulo'd in programming, but it is legit in math
+		        _value.push(parseInt((data[i] * -1) % modNum));
+		    }
+		    else {
+		        _value.push(parseInt(data[i] % modNum));
+		    }
 		}		
 		
 		return _value;
@@ -64,7 +145,7 @@ var Modulo = function(){
 var Logarithmic = function(){
 	
 	this.normalize = function(data,minValue,maxValue)
-	{	
+	{
 		var dataMin = Math.min.apply(Math,data);
 		var dataMax = 0.0000000001;//min number greater than zero
 		var _value = new Array();		
